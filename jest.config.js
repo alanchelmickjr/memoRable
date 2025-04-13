a07fc9767226c7c43d188dad47c8b5f224c4df0b
Alan <alanhelmick@icloud.com>
2025-04-13 16:58:30 -0700

    updated test cofig for publish npm

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,25 @@
 export default {
   testEnvironment: 'node',
-  transform: {},
+  transform: {
+    '^.+\\.js$': ['babel-jest', { configFile: './babel.config.js' }]
+  },
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   },
   testMatch: ['**/tests/**/*.test.js'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/index.js',
+    '!src/index.js'
   ],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80,
-    },
+      statements: 80
+    }
   },
   setupFiles: ['dotenv/config'],
-  verbose: true,
+  verbose: true
 };
